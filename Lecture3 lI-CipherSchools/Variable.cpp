3019f9345747e9304eea2f702d6f75568c5b72a3
Ritvik Bharti <122254722+ritvikbharti@users.noreply.github.com>
2023-10-30 20:39:06 +0530

    Add files via upload

---
+++ Lecture3 lI-CipherSchools/Variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout << a+b << endl << "a+b";
+    cout << a+b;
+    cout << endl;
+    cout << "a+b";
+    return 0;
+
+}
